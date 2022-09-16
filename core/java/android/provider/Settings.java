e7b53ac837a157b1d28e25ad0963cfc7061d0d6a
Trishiraj <gamerprince.exp@gmail.com>
2022-09-16 06:06:57 +0000

    New ReTicker Style [1/2]
    Style and Animation based on iPhone's new Dynamic Island feature.
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5644,6 +5644,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_REFRESH_INTERVAL = "network_traffic_refresh_interval";
 
         /**
+         * Restyled reTicker
+         * @hide
+         */
+        public static final String NEW_RETICKER = "new_reticker";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
